24349b610602c11d2d6bc78b641c77ed944bf73e
Tuuli Tiilikainen <tuulit@gmail.com>
2016-09-27 10:57:17 +0300

    renamed goodbye

--- src/modules/AppRouter.js
+++ src/modules/AppRouter.js
@@ -11,7 +11,7 @@ import MapViewContainer from './map/MapViewContainer';
 import TeamViewContainer from './team/TeamViewContainer';
 import WelcomeViewContainer from './welcome/WelcomeViewContainer';
 import CheckPointViewContainer from './checkpoints/CheckPointViewContainer';
-import GoodbyeViewContainer from './Goodbye/GoodbyeViewContainer';
+import GoodbyeViewContainer from './goodbye/GoodbyeViewContainer';
 import TeamPointsViewContainer from './teamPoints/TeamPointsViewContainer';
 
 /**
