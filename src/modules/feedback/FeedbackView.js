4b10e9dec32f249c0a47a2f5b56137ef4b498c48
Tuuli Tiilikainen <tuulit@gmail.com>
2016-11-07 21:39:31 +0200

    eslint and style fixing

--- src/modules/feedback/FeedbackView.js
+++ src/modules/feedback/FeedbackView.js
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-
 import {
   Text,
   View,
@@ -10,17 +9,20 @@ import {
   ScrollView
 } from 'react-native';
 
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
 
 import * as NavigationState from '../../modules/navigation/NavigationState';
 
 var radio_props = [
-  {label: '  Kyllä', value: 0 },
-  {label: '  Ei', value: 1 }
+  {label: ' Kyllä  ', value: 0},
+  {label: ' Ei  ', value: 1}
 ];
 
 const FeedbackView = React.createClass({
-
   propTypes: {
     dispatch: PropTypes.func.isRequired
   },
@@ -38,101 +40,86 @@ const FeedbackView = React.createClass({
     }));
   },
 
-  render: function() {
-      var _scrollView: ScrollView;
-       return (
-         <View style = {styles.container}>
-          <ScrollView ref={(scrollView) => { _scrollView = scrollView; }}
-            automaticallyAdjustContentInsets={false}
-            onScroll={() => { console.log('onScroll!'); }}
-            scrollEventThrottle={200}
-            style={styles.scrollView}>
-            <Text style={styles.baseText}>Super-Adan järjestäjät ovat kiinnostuneita kokemuksistanne tapahtumassa. Vastaamalla autat meitä tekemään tapahtumasta paremman!</Text>
-            <Text style={styles.boldText}>Anna kouluarvosana tapahtumalle (4-10):</Text>
+  render() {
+    var _scrollView: ScrollView;
+    return (
+     <View style = {styles.container}>
+      <ScrollView ref={(scrollView) => { _scrollView = scrollView; }}
+        automaticallyAdjustContentInsets={false}
+        onScroll={() => { console.log('onScroll!'); }}
+        scrollEventThrottle={200}
+        style={styles.scrollView}>
+        <Text style={styles.baseText}>Super-Adan järjestäjät ovat kiinnostuneita kokemuksistanne tapahtumassa. Vastaamalla autat meitä tekemään tapahtumasta paremman!</Text>
+        <Text style={styles.boldText}>Anna kouluarvosana tapahtumalle (4-10):</Text>
+        <View style={styles.inputs}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={[styles.input]}
+              />
+          </View>
+        </View>
+        <Text style={styles.boldText}>Mistä sait tiedon tapahtumasta?</Text>
+          <View style={styles.inputs}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={[styles.input]}
+                />
+            </View>
+          </View>
+          <Text style={styles.boldText}>Innostuitko IT-alasta?</Text>
+            <RadioForm
+              style={styles.button}
+              radio_props={radio_props}
+              initial={null}
+              onPress={(value) => {this.setState({value:value})}}
+              formHorizontal={true}
+              buttonColor={'#000000'}
+              />
+            <Text style={styles.boldText}>Muuttuiko käsityksesi IT-alasta?</Text>
+              <RadioForm
+                style={styles.button}
+                radio_props={radio_props}
+                initial={null}
+                onPress={(value) => {this.setState({value:value})}}
+                formHorizontal={true}
+                buttonColor={'#000000'}
+                />
+            <Text style={styles.boldText}>Voisitko kuvitella meneväsi IT-alalle töihin?</Text>
+              <RadioForm
+                style={styles.button}
+                radio_props={radio_props}
+                initial={null}
+                onPress={(value) => {this.setState({value:value})}}
+                formHorizontal={true}
+                buttonColor={'#000000'}
+                />
+            <Text style={styles.boldText}>Kehitysehdotuksia seuraavan Super-Ada -tapahtuman järjestäjille?</Text>
             <View style={styles.inputs}>
               <View style={styles.inputContainer}>
-            <TextInput
-                    style={[styles.input]}
-                    />
-
+                <TextInput
+                  style={[styles.input]}
+                  />
               </View>
+            </View>
+            <TouchableOpacity onPress={this.goodbyefeedback}>
+              <View style={styles.send}>
+                  <Text style={styles.whiteFont}>LÄHETÄ</Text>
               </View>
-              <Text style={styles.boldText}>Mistä sait tiedon tapahtumasta?</Text>
-              <View style={styles.inputs}>
-                <View style={styles.inputContainer}>
-              <TextInput
-                      style={[styles.input]}
-                      />
-
-                </View>
-                </View>
-              <Text style={styles.boldText}>Innostuitko IT-alasta?</Text>
-              <RadioForm
-                        style={styles.button}
-                        radio_props={radio_props}
-                        initial={null}
-                        onPress={(value) => {this.setState({value:value})}}
-                        formHorizontal={true}
-                        buttonColor={'#000000'}
-                      />
-
-              <Text style={styles.boldText}>Muuttuiko käsityksesi IT-alasta?</Text>
-              <RadioForm
-                        style={styles.button}
-                        radio_props={radio_props}
-                        initial={null}
-                        onPress={(value) => {this.setState({value:value})}}
-                        formHorizontal={true}
-                        buttonColor={'#000000'}
-                      />
-              <Text style={styles.boldText}>Voisitko kuvitella meneväsi IT-alalle töihin?</Text>
-              <RadioForm
-                    style={styles.button}
-                    radio_props={radio_props}
-                    initial={null}
-                    onPress={(value) => {this.setState({value:value})}}
-                    formHorizontal={true}
-                    buttonColor={'#000000'}
-                        />
-
-              <Text style={styles.boldText}>Kehitysehdotuksia seuraavan Super-Ada -tapahtuman järjestäjille?</Text>
-              <View style={styles.inputs}>
-                <View style={styles.inputContainer}>
-              <TextInput
-                      style={[styles.input]}
-                      />
-
-                </View>
-                </View>
-                
-              <TouchableOpacity onPress={this.goodbyefeedback}>
-                <View style={styles.send}>
-                    <Text style={styles.whiteFont}>LÄHETÄ</Text>
-                </View>
-              </TouchableOpacity>
-
+            </TouchableOpacity>
           </ScrollView>
-          </View>
-
-
-
+        </View>
     );
-
   }
-
 });
-
-
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-      flex: 1,
-      backgroundColor: 'transparent'
+    flex: 1,
+    backgroundColor: 'transparent'
   },
   scrollView: {
     backgroundColor: 'rgba(245,245,245,1)',
-
     flex: 1
   },
   button: {
@@ -147,7 +134,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 20,
     color: 'black'
-
   },
   boldText: {
     marginLeft: 10,
@@ -156,41 +142,38 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: 'black'
-
   },
   input: {
-      position: 'absolute',
-      left: 10,
-      right: 10,
-      fontSize: 16,
-      height: 50,
-      paddingBottom: 0
-
+    position: 'absolute',
+    left: 10,
+    right: 10,
+    fontSize: 16,
+    height: 50,
+    paddingBottom: 0
   },
   inputs: {
     marginTop: 2,
     marginBottom: 2,
-      flex: .25
+    flex: .25
   },
   inputContainer: {
-      padding: 0,
-      borderWidth: 1,
-      borderColor: 'transparent',
-      height: 45
+    padding: 0,
+    borderWidth: 1,
+    borderColor: 'transparent',
+    height: 45
   },
   send: {
-      backgroundColor: '#ff5454',
-      padding: 20,
-      marginTop: 40,
-      marginLeft: 30,
-      marginRight: 30,
-      marginBottom: 30,
-      alignItems: 'center'
+    backgroundColor: '#ff5454',
+    padding: 20,
+    marginTop: 40,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30,
+    alignItems: 'center'
   },
   whiteFont: {
     color: '#FFF',
     fontSize: 18
   }
-
 });
 export default FeedbackView;
