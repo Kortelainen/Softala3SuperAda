04571e13837c8879930eccf6eb22a8b194d9eceb
Tuuli Tiilikainen <tuulit@gmail.com>
2016-11-07 21:50:36 +0200

    eslint and style fixing

--- src/modules/goodbyeFeedback/GoodbyeFeedbackView.js
+++ src/modules/goodbyeFeedback/GoodbyeFeedbackView.js
@@ -1,90 +1,78 @@
 import React, {PropTypes} from 'react';
-
 import {
   Text,
   View,
   StyleSheet,
-  TextInput,
-  Image,
-  TouchableOpacity
+  Image
 } from 'react-native';
 
 import * as NavigationState from '../../modules/navigation/NavigationState';
 
 const GoodbyeFeedbackView = React.createClass({
-
   propTypes: {
     dispatch: PropTypes.func.isRequired
   },
 
-  getInitialState: function() {
+  getInitialState() {
     return {
-
-      background: `rgba(250,155,145,1)`
-    }
+      background: 'rgba(255,0,54,1)'
+    };
   },
 
-  render: function() {
-
-
-
+  render() {
     return (
-
-
       <View style={[styles.container, {backgroundColor: this.state.background}]}>
-      <View style={styles.header}>
-              <Image style={styles.mark} source={require('../../../images/kiitos.png')}/>
-          </View>
-
-            <View style={styles.inputContainer}>
-               <Text style={styles.textstyle}>Kiitos palautteestasi!</Text>
-               <Text style={styles.textstyle}>Tervetuloa ensi vuonna uudestaan!</Text>
-
-            </View>
-            <View style={styles.inputContainer}>
-                <Text style={styles.bottomText}>t- Super-Ada tiimi</Text>
-
-
-                </View>
+        <View style={styles.header}>
+          <Text style={styles.titleText}>
+            Kiitos!
+          </Text>
+          <Image style={styles.mark} source={require('../../../images/kiitos.png')}/>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.textstyle}>Kiitos palautteestasi!</Text>
+          <Text style={styles.textstyle}>Tervetuloa ensi vuonna uudestaan!</Text>
+        </View>
+        <View style={styles.regardContainer}>
+          <Text style={styles.bottomText}>t- Super-Ada tiimi</Text>
+        </View>
       </View>
-
     );
-
   }
-
 });
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-      flex: 1,
-      backgroundColor: 'transparent'
+    flex: 1,
+    backgroundColor: 'transparent'
   },
-
-      header: {
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          flex: 0,
-          backgroundColor: 'transparent'
-      },
-      mark: {
-          width: 200,
-          height: 250
-      },
-
-
-      textstyle: {
-        color: '#FFF',
-        marginBottom: 15,
-        fontSize: 20,
-        textAlign: 'center'
-      },
-      bottomText: {
-        color: '#FFF',
-        marginTop: 150,
-        fontSize: 20,
-        textAlign: 'center'
-
-      }
+  header: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  mark: {
+    width: 200,
+    height: 250
+  },
+  textstyle: {
+    color: '#FFF',
+    marginBottom: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  bottomText: {
+    color: '#FFF',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 50
+  }
 });
 export default GoodbyeFeedbackView;
