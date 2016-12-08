ee40f324a78b4776ca7c4fb75b5a32dbaa987380
Rasmus Eskola <fruitiex@gmail.com>
2016-12-08 14:04:49 +0200

    Fix image picker by adding camera & gallery permissions back

--- android/app/src/main/java/com/softala3superada/MainApplication.java
+++ android/app/src/main/java/com/softala3superada/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTSplashScreenPackage(),
             new PhotoViewPackage(),
             new ImagePickerPackage()
       );
