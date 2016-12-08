a2528944ad0173c9dd42b836fd51af33ec9973e7
Rasmus Eskola <fruitiex@gmail.com>
2016-12-08 13:16:16 +0200

    Upgrade libs, get rid of react-native-lock

--- android/app/src/main/java/com/softala3superada/MainApplication.java
+++ android/app/src/main/java/com/softala3superada/MainApplication.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.reactnative.photoview.PhotoViewPackage;
-import com.auth0.lock.react.LockReactPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new PhotoViewPackage(),
-            new LockReactPackage(),
             new ImagePickerPackage()
       );
     }
